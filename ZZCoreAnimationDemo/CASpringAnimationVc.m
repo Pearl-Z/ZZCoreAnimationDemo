edf99ea34d5371a74cb13d2873ed4936b6b3d711
谢琛舟 <747297597@qq.com>
2016-08-03 17:19:46 +0800

    添加iOS9.0以下 spring 示例

--- ZZCoreAnimationDemo/CASpringAnimationVc.m
+++ ZZCoreAnimationDemo/CASpringAnimationVc.m
@@ -13,6 +13,8 @@
 
 @property(nonatomic,strong) CALayer *imageLayer;
 
+@property(nonatomic,strong) UIImageView *imgView;
+
 @end
 
 @implementation CASpringAnimationVc
@@ -24,27 +26,46 @@
     // Do any additional setup after loading the view.
     
     self.view.backgroundColor = [UIColor whiteColor];
-    
-    _imageLayer = [CALayer layer];
-    // 设置该CALayer的边框、大小、位置等属性
-    _imageLayer.cornerRadius = 6;
-    _imageLayer.borderWidth = 1;
-    _imageLayer.borderColor = [UIColor blackColor].CGColor;
-    _imageLayer.masksToBounds = YES;
-    _imageLayer.allowsEdgeAntialiasing = YES;
-    _imageLayer.frame = CGRectMake(50, 200, 100, 135);
-    // 设置该imageLayer显示的图片
-    _imageLayer.contents = (id)[[UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"img1" ofType:@"jpeg"]] CGImage];
-    [self.view.layer addSublayer:_imageLayer];
+
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] < 9.0){
+        _imgView = [UIImageView new];
+        _imgView.frame = CGRectMake(50, 200, 100, 135);
+        _imgView.image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"img1" ofType:@"jpeg"]];
+        [self.view addSubview:_imgView];
+    }else{
+        _imageLayer = [CALayer layer];
+        // 设置该CALayer的边框、大小、位置等属性
+        _imageLayer.cornerRadius = 6;
+        _imageLayer.borderWidth = 1;
+        _imageLayer.borderColor = [UIColor blackColor].CGColor;
+        _imageLayer.masksToBounds = YES;
+        _imageLayer.allowsEdgeAntialiasing = YES;
+        _imageLayer.frame = CGRectMake(50, 200, 100, 135);
+        // 设置该imageLayer显示的图片
+        _imageLayer.contents = (id)[[UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"img1" ofType:@"jpeg"]] CGImage];
+        [self.view.layer addSublayer:_imageLayer];
+    }
     
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-     NSLog(@"%@",[NSValue valueWithCGRect:_imageLayer.frame]);
-    [_imageLayer zz_springAnimationWithMass:1 stiffness:100 damping:5 initialVelocity:0 basicAnimationType:ZZBasicAnimationTypeTranslation coordinates:ZZCoordinatesMake(150, 0, 0) angle:0 completedBlock:^(BOOL flag) {
-        NSLog(@"%@",[NSValue valueWithCGRect:_imageLayer.frame]);
-    }];
-    _imageLayer.transform = CATransform3DIdentity;
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] < 9.0) {
+        [UIView animateWithDuration:5 delay:0 usingSpringWithDamping:0.2 initialSpringVelocity:5 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            CGRect frame = self.imgView.frame;
+            frame.origin.x += 150;
+            self.imgView.frame = frame;
+        } completion:^(BOOL finished) {
+            CGRect frame = self.imgView.frame;
+            frame.origin.x -= 150;
+            self.imgView.frame = frame;
+        }];
+    }else{
+        [_imageLayer zz_springAnimationWithMass:1 stiffness:100 damping:5 initialVelocity:0 basicAnimationType:ZZBasicAnimationTypeTranslation coordinates:ZZCoordinatesMake(150, 0, 0) angle:0 completedBlock:^(BOOL flag) {
+        }];
+        _imageLayer.transform = CATransform3DIdentity;
+    }
+    
     
 }
 
